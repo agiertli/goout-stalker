3b8abb9f18c3ceaed2939a799333699ac5276d30
Anton Giertli <agiertli@redhat.com>
2018-12-24 17:33:30 +0100

    restructure

--- src/main/java/org/goout/stalker/rest/Activator.java
+++
@@ -1,9 +0,0 @@
-package org.goout.stalker.rest;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class Activator extends Application {
-
-}
