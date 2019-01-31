79f7f2fe712029b523b71be3f32c5ddd3e179298
Anton Giertli <agiertli@redhat.com>
2019-01-31 15:18:53 +0100

    fixing s2i

--- goout-stalker-client/clientsite/nginx-start/prepare_env.sh
+++ goout-stalker-client/clientsite/nginx-start/prepare_env.sh
@@ -1,2 +1,1 @@
-#sed -i -e "s/.*var BASE_URL=\"http:\/\/localhost:8080\/api\";.*/var BASE_URL=\"http:\/\/$BACKEND_HOSTNAME\/api\" /" ../env.js
-echo "revert"
+sed -i -e "s/.*var BASE_URL=\"http:\/\/localhost:8080\/api\";.*/var BASE_URL=\"http:\/\/$BACKEND_HOSTNAME\/api\" /" /opt/app-root/src/env.js
